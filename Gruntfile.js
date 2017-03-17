c5c13bb8c032e84f44b0ba03a5e0952b8cf7d886
Billy Ernest <bernest@gridprotectionalliance.org>
2017-03-17 14:58:19 -0400

    Added grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,65 @@
-module.exports = (grunt) => {
-  require('load-grunt-tasks')(grunt);
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt)
 
-  grunt.loadNpmTasks('grunt-execute');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-execute')
+    grunt.loadNpmTasks('grunt-contrib-clean')
+    grunt.loadNpmTasks('grunt-ts')
 
-  grunt.initConfig({
+    grunt.initConfig({
+        clean: {
+            dist: ['dist']
+        },
+        copy: {
+            src_to_dist: {
+                cwd: 'src',
+                expand: true,
+                src: ['**/*', '!**/*.js', '!**/External/*', '!**/*.ts', '!**/*.scss', '!img/*', "!docs/*"],
+                dest: 'dist'
+            },
+            img_to_dist: {
+                cwd: 'src',
+                expand: true,
+                src: ['img/**/*'],
+                dest: 'dist/'
+            },
+            docs_to_dist: {
+                expand: true,
+                src: ['docs/**/*'],
+                dest: 'dist/'
+            },
+            pluginDef: {
+                expand: true,
+                src: ['README.md', "LICENSE"],
+                dest: 'dist/'
+            }
+        },
 
-    clean: ['dist'],
+        watch: {
+            rebuild_all: {
+                files: ['src/**/*', 'plugin.json'],
+                tasks: ['default'],
+                options: { spawn: false }
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['es2015']
+            },
+            dist: {
+                options: {
+                    plugins: ['transform-es2015-modules-systemjs', 'transform-es2015-for-of']
+                },
+                files: [{
+                    cwd: 'src',
+                    expand: true,
+                    src: ['**/*.js'],
+                    dest: 'dist',
+                    ext: '.js'
+                }]
+            }
+        }
+    })
 
-    copy: {
-      src_to_dist: {
-        cwd: 'src',
-        expand: true,
-        src: ['**/*', '!**/*.js', '!**/*.scss', '!img/**/*'],
-        dest: 'dist'
-      },
-      pluginDef: {
-        expand: true,
-        src: ['README.md'],
-        dest: 'dist',
-      },
-      externals: {
-        cwd: 'src',
-        expand: true,
-        src: ['**/external/*'],
-        dest: 'dist'
-      },
-      img_to_dist: {
-        cwd: 'src',
-        expand: true,
-        src: ['img/**/*'],
-        dest: 'dist'
-      }
-    },
-
-    watch: {
-      rebuild_all: {
-        files: ['src/**/*'],
-        tasks: ['default'],
-        options: {spawn: false}
-      },
-    },
-
-    babel: {
-      options: {
-        sourceMap: true,
-        presets: ['es2015'],
-        plugins: ['transform-es2015-modules-systemjs', 'transform-es2015-for-of'],
-      },
-      dist: {
-        files: [{
-          cwd: 'src',
-          expand: true,
-          src: ['*.js'],
-          dest: 'dist',
-          ext: '.js'
-        }]
-      },
-    },
-
-  });
-
-  grunt.registerTask('default', ['clean', 'copy:src_to_dist', 'copy:pluginDef', 'copy:img_to_dist', 'copy:externals', 'babel']);
-};
+    grunt.registerTask('default', ['clean', 'copy:src_to_dist', 'copy:img_to_dist', 'copy:docs_to_dist', 'copy:pluginDef', 'babel'])
+}
